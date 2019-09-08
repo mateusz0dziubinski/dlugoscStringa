688b204cab240044c1a8a62ff02df3c96ae9cef6
mateusz0dziubinski <mateusz0dziubinski@o2.pl>
2019-09-08 12:54:52 +0200

    Zadanie: zrobione

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var text1 = "Uwielbiam JavaScript";
+var text2 = "Jestem świetnym programistą"
+
+var dlugoscTextu1 = text1.length;
+var dlugoscTextu2 = text2.length;
+
+if (dlugoscTextu2>dlugoscTextu1){
+    console.log(text2)
+}
+else {
+    console.log("Text 1 nie jest dłuższy od textu2")
+}
+
+
