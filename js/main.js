3ef6d429f6a3739f89bd0712fd5469675db002d5
mateusz0dziubinski <mateusz0dziubinski@o2.pl>
2019-09-10 07:39:27 +0200

    Zadanie: zrobione

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 var text1 = "Uwielbiam JavaScript";
-var text2 = "Jestem świetnym programistą"
+var text2 = "Jestem świetnym programistą";
+
+var dlugoscTextu1 = text1;
+var dlugoscTextu2 = text2;
 
 var dlugoscTextu1 = text1.length;
 var dlugoscTextu2 = text2.length;
 
-if (dlugoscTextu2>dlugoscTextu1){
-    console.log(text2)
-}
-else {
-    console.log("Text 1 nie jest dłuższy od textu2")
+function dlugoscStringa(dlugoscTextu1, dlugoscTextu2){
+    if (text1.length < text2.length){
+        console.log(text2)
+
+    }else{
+        console.log("Text 1 nie jest dłuższy od textu2")
+    }
 }
 
+console.log(dlugoscStringa());
 
+
+/*
+"Jestem świetnym programistą";
+"Uwielbiam JavaScript";
+*/
